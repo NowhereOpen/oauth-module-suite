949be087d11df8b3f164999d0423eac55bf0bebd
chulman444 <chulman444@gmail.com>
2020-04-30 19:11:38 +0900

    Before build

---
+++ index.ts
@@ -0,0 +1,8 @@
+export { Bitbucket } from "./src/cred-modules/bitbucket-oauth2"
+export { Facebook } from "./src/cred-modules/facebook-oauth2"
+export { Github } from "./src/cred-modules/github-oauth2"
+export { Google } from "./src/cred-modules/google-oauth2"
+export { Reddit } from "./src/cred-modules/reddit-oauth2"
+export { Trello } from "./src/cred-modules/trello-oauth1"
+export { Twitch } from "./src/cred-modules/twitch-oauth2"
+export { Twitter } from "./src/cred-modules/twitter-oauth1"
